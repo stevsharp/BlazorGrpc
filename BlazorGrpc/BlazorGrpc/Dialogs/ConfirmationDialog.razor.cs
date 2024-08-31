90d3f9863d2e298da4eb21f46d2390e3f802412c
SPYROS\Spyros <sponaris@gmail.com>
2024-08-31 19:05:49 +0300

    Dialogs

---
+++ BlazorGrpc/BlazorGrpc/Dialogs/ConfirmationDialog.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace BlazorGrpc.Dialogs;
+
+public partial class ConfirmationDialog
+{
+    [CascadingParameter]
+    private MudDialogInstance MudDialog { get; set; } = default!;
+
+    [Parameter]
+    public string? ContentText { get; set; }
+
+    private void Submit()
+    {
+        MudDialog.Close(DialogResult.Ok(true));
+    }
+
+    private void Cancel()
+    {
+        MudDialog.Cancel();
+    }
+}
---
+++ BlazorGrpc/BlazorGrpc/Dialogs/DeleteConfirmation.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace BlazorGrpc.Dialogs;
+
+public partial class DeleteConfirmation
+{
+    [CascadingParameter] 
+    private MudDialogInstance MudDialog { get; set; } = default!;
+
+    [EditorRequired]
+    [Parameter] 
+    public string? ContentText { get; set; }
+
+    private Task Submit()
+    {
+
+        return Task.CompletedTask;
+    }
+
+    private void Cancel()
+    {
+        MudDialog.Cancel();
+    }
+}
